4ddb46a566c23790d1268e795b68dcc224b8cbac
CyrussT <24277152+CyrussT@users.noreply.github.com>
2021-07-15 00:33:01 +0800

    Update certs_split.py
    - added catching of invalid characters for directory, trimming of white-spaces
    - checking to ensure user inputs are valid (still have to take care of cases where users enter format)
    - added logging to terminal and text
    - a bit of error handling?
    -  confirmation dialog after program finish running to review output

--- certs_split.py
+++ certs_split.py
@@ -1,27 +1,76 @@
 import PyPDF2
 import csv
 import os
+import time
+import logging
 
+from PyPDF2.utils import PdfReadError, PyPdfError
+
+# array to store the names and companies
 names = list()
+# array to filter out characters to remove
+unwanted_chars = ["/", "\t", ":", "!", ";"]
 
-pdfName = input("Please input merged pdf name: ")
-csvName = input("Please enter csv name: ")
+#setting up of logs
+clogs = logging.getLogger(__name__)
+clogs.setLevel(logging.DEBUG)
+
+# handler 1 - writer
+file = logging.FileHandler("certs.log")
+file.setLevel(logging.DEBUG)
+fileformat = logging.Formatter("%(asctime)s:%(levelname)s:%(message)s",datefmt="%H:%M:%S")
+file.setFormatter(fileformat)
+
+# handler 2 - to stream
+stream = logging.StreamHandler()
+streamformat = logging.Formatter("%(levelname)s:%(module)s:%(message)s")
+stream.setFormatter(streamformat)
+
+clogs.addHandler(file)
+clogs.addHandler(stream)
+# end of logging 
+
+# request for file name & check if valid if not raise errors
+pdfName = input("Please input Merged PDF file name (excluding the extension .pdf): ")
+if not os.path.exists(pdfName+".pdf"):
+    clogs.critical("Unable to find file " + pdfName + ".pdf!")
+    raise FileNotFoundError
+csvName = input("Please enter csv name (excluding the extension .csv): ")
+if not os.path.exists(csvName+".csv"):
+    clogs.critical("Unable to find file " + csvName + ".csv!")
+    raise FileNotFoundError
 folderName = input("Please enter folder name (make sure you have created the folder): ")
+if not os.path.isdir("./" + folderName + "/"):
+    clogs.critical("Unable to find folder: " + folderName)
+    raise NotADirectoryError
 
+# open csv file and add to list
 with open("./"+ csvName + ".csv") as file:
     reader = csv.reader(file)
     for row in reader:
+        #To get rid of the unwanted characters from unwanted_chars list
+        for char in unwanted_chars:
+            row[0] = row[0].replace(char, "").strip()
+            row[1] = row[1].replace(char, "").strip()
         names.append(row)
 
-print("Successfully loaded all names")
+clogs.info("Successfully loaded " + str(len(names)) + " names!")
 
-file = open("./"+ pdfName +".pdf", "rb")
+# open pdf file
+try:
+    file = open("./"+ pdfName +".pdf", "rb")
+except PyPdfError as e:
+    clogs.critical("PyPDF Error: " + e)
+    raise PyPdfError
 
 infile = PyPDF2.PdfFileReader(file)
 outfile = PyPDF2.PdfFileWriter()
 
+# for each page in the merge file, retrieve the page at x position and save it as a new file
+# named as names[0] and directory name under the company as names[1]
+
 for page in range(infile.getNumPages()):
-    print("Generating page: " + str(page+1))
+    clogs.info("Generating page: " + str(page+1) + ", for " + names[page][0])
     current = infile.getPage(page)
     outfile = PyPDF2.PdfFileWriter()
     outfile.addPage(current)
@@ -30,9 +79,15 @@ for page in range(infile.getNumPages()):
     file_name = names[page][0] +".pdf"
 
     if not os.path.exists(write_dir):
-        os.makedirs(write_dir)
-
+        try:
+            os.makedirs(write_dir)
+        except OSError as e:
+            clogs.critical("Error: " + e)
+            raise
+    # write to outfile
     with open(write_dir + file_name, "wb") as outstream:
         outfile.write(outstream)
 
 file.close()
+clogs.info("Succesfully generated " + str(len(names)) + " certificates!")
+x = input("Press enter to close the program! ")
